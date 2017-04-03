04d264b2ffa26465579aacba9c7d0af2c207b014
Deimante <d.kupraityte@gmail.com>
2017-04-03 11:10:55 +0300

    Kad rodytu klaidos pranesima jeigu nieko neivedi

--- app/src/main/java/com/example/moksleivis/kavine/MainActivity.java
+++ app/src/main/java/com/example/moksleivis/kavine/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private String username;
     private String password;
 
-
+//jj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
